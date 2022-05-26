341c5a3d1af3a5701393ade2a62f9684cc8aab45
Dương Quang Vinh <duongquangvinh6@gmail.com>
2022-05-26 17:57:52 +0700

    update Header + Footer

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,22 +4,59 @@ import { Box, Button, Toolbar, Typography } from '@mui/material'
 import IconButton from '@mui/material/IconButton'
 import MenuIcon from '@mui/icons-material/Menu'
 import React from 'react'
-
+import Badge from "@mui/material/Badge";
+import MailOutlineIcon from "@mui/icons-material/MailOutline";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import Avatar from "@mui/material/Avatar";
 
 interface HeaderProps {}
 const Header = (props: HeaderProps) => {
 
+function stringToColor(string:any) {
+	let hash = 0;
+	let i;
+	for (i = 0; i < string.length; i += 1) {
+		hash = string.charCodeAt(i) + ((hash << 5) - hash);
+	}
+	let color = "#";
+	for (i = 0; i < 3; i += 1) {
+		const value = (hash >> (i * 8)) & 0xff;
+		color += `00${value.toString(16)}`.slice(-2);
+	}
+	return color;
+}
+function stringAvatar(name:any) {
+	return {
+		sx: {
+			bgcolor: stringToColor(name),
+		},
+		children: `${name.split(" ")[0][0]}${name.split(" ")[1][0]}`,
+	};
+}
+
   return (
-    <Box sx={{flexGrow: 1}} >
+    <Box sx={{my:"20px"}} >
       <AppBar position="static" elevation={0} color="transparent">
-        <Toolbar >
+        <Toolbar sx={{dislay: "flex", gap:"20px"}}>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1,  '-webkit-user-select':'none', 'user-select': 'none', fontWeight:"700", letterSpacing:"3px"}}>
             Evenity
           </Typography>
-          <Button variant='text' sx={{color:"#000000"}}>Login</Button>
-          <Button variant='text' sx={{margin:"20px auto", color:"#000000"}}>Sign up</Button>
+          <Button variant='text' sx={{color:"#000000"}} >Login</Button>
+          <Button variant='text' sx={{color:"#000000"}}>Sign up</Button>
           <Button variant='text' sx={{color:"#6A35F2"}}>create an event</Button>
-         
+          <IconButton>
+            <Badge badgeContent={2} sx={{color:"#6A35F2", "&  span":{backgroundColor: "#6A35F2", color:"#ffffff"}}}>
+              <MailOutlineIcon />
+            </Badge>
+          </IconButton>
+          <IconButton>
+            <Badge badgeContent={2} sx={{color:"#6A35F2", "&  span":{backgroundColor: "#6A35F2", color:"#ffffff"}}}>
+              <NotificationsNoneIcon />
+            </Badge>
+          </IconButton>
+          <IconButton>
+            <Avatar {...stringAvatar('Vinh Duong Quang')} />
+          </IconButton>
         </Toolbar>
       </AppBar>
     </Box>
--- components/HomePageControl.tsx
+++ components/HomePageControl.tsx
@@ -8,168 +8,168 @@ import Select from "@mui/material/Select";
 interface TicketListProps {}
 
 const options = [
-  { label: "Current", id: 1 },
-  { label: "Pulp Fiction", id: 2 },
-  { label: "Pulp Fiction 3", id: 3 },
+	{ label: "Current", id: 1 },
+	{ label: "Pulp Fiction", id: 2 },
+	{ label: "Pulp Fiction 3", id: 3 },
 ];
 
 const currentOptions = [
-  { label: "Current Event", id: 1 },
-  { label: "Pass Event", id: 2 },
-  { label: "Future Event", id: 3 },
+	{ label: "Current Event", id: 1 },
+	{ label: "Pass Event", id: 2 },
+	{ label: "Future Event", id: 3 },
 ];
 
 const TicketList = (props: TicketListProps) => {
-  const [dateOption, setDateOption] = React.useState("");
-  const [typeOption, setTypeOption] = React.useState("");
-  const [categoryOption, setCategoryOption] = React.useState("");
-  const [openDateForm, setOpenDateForm] = React.useState(false);
-  const [openTypeForm, setOpenTypeForm] = React.useState(false);
-  const [openCategoryForm, setOpenCategoryForm] = React.useState(false);
+	const [dateOption, setDateOption] = React.useState("");
+	const [typeOption, setTypeOption] = React.useState("");
+	const [categoryOption, setCategoryOption] = React.useState("");
+	const [openDateForm, setOpenDateForm] = React.useState(false);
+	const [openTypeForm, setOpenTypeForm] = React.useState(false);
+	const [openCategoryForm, setOpenCategoryForm] = React.useState(false);
 
-  const handleChangeDateForm = (event: any) => {
-    setDateOption(event.target.value);
-  };
-  const handleCloseDateForm = () => {
-    setOpenDateForm(false);
-  };
+	const handleChangeDateForm = (event: any) => {
+		setDateOption(event.target.value);
+	};
+	const handleCloseDateForm = () => {
+		setOpenDateForm(false);
+	};
 
-  const handleOpenDateForm = () => {
-    setOpenDateForm(true);
-  };
+	const handleOpenDateForm = () => {
+		setOpenDateForm(true);
+	};
 
-  const handleChangeTypeForm = (event: any) => {
-    setTypeOption(event.target.value);
-  };
-  const handleCloseTypeForm = () => {
-    setOpenTypeForm(false);
-  };
+	const handleChangeTypeForm = (event: any) => {
+		setTypeOption(event.target.value);
+	};
+	const handleCloseTypeForm = () => {
+		setOpenTypeForm(false);
+	};
 
-  const handleOpenTypeForm = () => {
-    setOpenTypeForm(true);
-  };
+	const handleOpenTypeForm = () => {
+		setOpenTypeForm(true);
+	};
 
-  const handleChangeCategoryForm = (event: any) => {
-    setCategoryOption(event.target.value);
-  };
-  const handleCloseCategoryForm = () => {
-    setOpenCategoryForm(false);
-  };
+	const handleChangeCategoryForm = (event: any) => {
+		setCategoryOption(event.target.value);
+	};
+	const handleCloseCategoryForm = () => {
+		setOpenCategoryForm(false);
+	};
 
-  const handleOpenCategoryForm = () => {
-    setOpenCategoryForm(true);
-  };
+	const handleOpenCategoryForm = () => {
+		setOpenCategoryForm(true);
+	};
 
-  return (
-    <>
-      <Box sx={{ mt: "170px", width: "100%" }}>
-        <Box sx={{ width: "80%", mx: "auto", display: "flex" }}>
-          <Typography
-            component="h2"
-            sx={{
-              fontSize: "2.1rem",
-              fontWeight: "700",
-              alignSelf: "center",
-              mr: "100px",
-            }}
-            flexShrink={0}
-          >
-            Upcoming Events
-          </Typography>
-          <Box
-            sx={{
-              display: "flex",
-              gap: "100px",
-              "& > div *": {
-                borderColor: "#6A35F2",
-                color: "#6A35F2",
-                fontWeight: "500",
-              },
-              "& > div label": { color: "#6A35F2", fontWeight: "500" },
-              "& > div .MuiInputBase-input": {
-                color: "#6A35F2",
-                fontWeight: "500",
-              },
-            }}
-          >
-            {/*Date Form*/}
-            <FormControl
-              sx={{
-                m: 1,
-                width: 250,
-              }}
-            >
-              <InputLabel id="date-controlled-open-select-label">
-                Current
-              </InputLabel>
-              <Select
-                labelId="date-controlled-open-select-label"
-                id="date-controlled-open-select"
-                open={openDateForm}
-                onClose={handleCloseDateForm}
-                onOpen={handleOpenDateForm}
-                value={dateOption}
-                label="Current"
-                onChange={handleChangeDateForm}
-              >
-                {currentOptions.map((option) => (
-                  <MenuItem key={option.id} value={option.label}>
-                    {option.label}
-                  </MenuItem>
-                ))}
-              </Select>
-            </FormControl>
+	return (
+		<>
+			<Box sx={{ mt: "170px", width: "100%" }}>
+				<Box sx={{ width: "80%", mx: "auto", display: "flex" }}>
+					<Typography
+						component="h2"
+						sx={{
+							fontSize: "2.1rem",
+							fontWeight: "700",
+							alignSelf: "center",
+							mr: "100px",
+						}}
+						flexShrink={0}
+					>
+						Upcoming Events
+					</Typography>
+					<Box
+						sx={{
+							display: "flex",
+							gap: "100px",
+							"& > div *": {
+								borderColor: "#6A35F2",
+								color: "#6A35F2",
+								fontWeight: "500",
+							},
+							"& > div label": { color: "#6A35F2", fontWeight: "500" },
+							"& > div .MuiInputBase-input": {
+								color: "#6A35F2",
+								fontWeight: "500",
+							},
+						}}
+					>
+						{/*Date Form*/}
+						<FormControl
+							sx={{
+								m: 1,
+								width: 250,
+							}}
+						>
+							<InputLabel id="date-controlled-open-select-label">
+								Current
+							</InputLabel>
+							<Select
+								labelId="date-controlled-open-select-label"
+								id="date-controlled-open-select"
+								open={openDateForm}
+								onClose={handleCloseDateForm}
+								onOpen={handleOpenDateForm}
+								value={dateOption}
+								label="Current"
+								onChange={handleChangeDateForm}
+							>
+								{currentOptions.map((option) => (
+									<MenuItem key={option.id} value={option.label}>
+										{option.label}
+									</MenuItem>
+								))}
+							</Select>
+						</FormControl>
 
-            {/*Type Form*/}
-            <FormControl sx={{ m: 1, width: 250 }}>
-              <InputLabel id="type-controlled-open-select-label">
-                Event Type
-              </InputLabel>
-              <Select
-                labelId="type-controlled-open-select-label"
-                id="type-controlled-open-select"
-                open={openTypeForm}
-                onClose={handleCloseTypeForm}
-                onOpen={handleOpenTypeForm}
-                value={typeOption}
-                label="Event Type"
-                onChange={handleChangeTypeForm}
-              >
-                {options.map((option) => (
-                  <MenuItem key={option.id} value={option.label}>
-                    {option.label}
-                  </MenuItem>
-                ))}
-              </Select>
-            </FormControl>
+						{/*Type Form*/}
+						<FormControl sx={{ m: 1, width: 250 }}>
+							<InputLabel id="type-controlled-open-select-label">
+								Event Type
+							</InputLabel>
+							<Select
+								labelId="type-controlled-open-select-label"
+								id="type-controlled-open-select"
+								open={openTypeForm}
+								onClose={handleCloseTypeForm}
+								onOpen={handleOpenTypeForm}
+								value={typeOption}
+								label="Event Type"
+								onChange={handleChangeTypeForm}
+							>
+								{options.map((option) => (
+									<MenuItem key={option.id} value={option.label}>
+										{option.label}
+									</MenuItem>
+								))}
+							</Select>
+						</FormControl>
 
-            {/*Category Form*/}
-            <FormControl sx={{ m: 1, width: "30%" }}>
-              <InputLabel id="category-controlled-open-select-label">
-                Category
-              </InputLabel>
-              <Select
-                labelId="category-controlled-open-select-label"
-                id="category-controlled-open-select"
-                open={openCategoryForm}
-                onClose={handleCloseCategoryForm}
-                onOpen={handleOpenCategoryForm}
-                value={categoryOption}
-                label="Category"
-                onChange={handleChangeCategoryForm}
-              >
-                {options.map((option) => (
-                  <MenuItem key={option.id} value={option.label}>
-                    {option.label}
-                  </MenuItem>
-                ))}
-              </Select>
-            </FormControl>
-          </Box>
-        </Box>
-      </Box>
-    </>
-  );
+						{/*Category Form*/}
+						<FormControl sx={{ m: 1, width: "30%" }}>
+							<InputLabel id="category-controlled-open-select-label">
+								Category
+							</InputLabel>
+							<Select
+								labelId="category-controlled-open-select-label"
+								id="category-controlled-open-select"
+								open={openCategoryForm}
+								onClose={handleCloseCategoryForm}
+								onOpen={handleOpenCategoryForm}
+								value={categoryOption}
+								label="Category"
+								onChange={handleChangeCategoryForm}
+							>
+								{options.map((option) => (
+									<MenuItem key={option.id} value={option.label}>
+										{option.label}
+									</MenuItem>
+								))}
+							</Select>
+						</FormControl>
+					</Box>
+				</Box>
+			</Box>
+		</>
+	);
 };
 
 export default TicketList;
