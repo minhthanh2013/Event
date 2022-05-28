5d0a90e1b30fda93a5df4bdae80e3baad365a8e2
Thanh Do <minhthanhd013@gmail.com>
2022-05-28 13:02:49 +0700

    test microservice

--- src/backend/ticket/dto/update-ticket.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateTicketDto } from './create-ticket.dto';
-
-export class UpdateTicketDto extends PartialType(CreateTicketDto) {}
--- src/backend/ticket/ticket.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TicketService } from './ticket.service';
-import { TicketController } from './ticket.controller';
-
-@Module({
-  controllers: [TicketController],
-  providers: [TicketService]
-})
-export class TicketModule {}
