fba93eb6c033334a65b28f49eb0208657b1bdc92
Thanh Do <minhthanhd013@gmail.com>
2022-05-30 23:02:19 +0700

    Update core-api/main.ts

--- core-api/src/main.ts
+++ core-api/src/main.ts
@@ -4,14 +4,15 @@ import { appendFile } from 'fs';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.connectMicroservice({
-    transport: Transport.TCP,
-    options: {
-      port: 3001
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port: 3001
+      }
     }
-  })
-  await app.startAllMicroservices();
-  await app.listen(3001);
+    ); 
+    app.listen();
 }
 bootstrap();
