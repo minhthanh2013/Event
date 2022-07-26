4ff9e7c7640e1a6e22fc80a2ee632aea97a0c974
Thanh Do <minhthanhd013@gmail.com>
2022-07-26 20:26:00 +0700

    Delete email controller

--- core-api/src/email/email.module.ts
+++ core-api/src/email/email.module.ts
@@ -1,12 +1,11 @@
 /* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { EmailController } from './email.controller';
 import { EmailService } from './email.service';
 
 @Module({
   imports: [ConfigModule],
   providers: [EmailService, ConfigService],
-  controllers: [EmailController],
+  controllers: [],
 })
 export class EmailModule {}
