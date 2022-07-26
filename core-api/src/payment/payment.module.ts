c65b2c1aeccc37a7df0d3a35955bf6afab79886b
Thanh Do <minhthanhd013@gmail.com>
2022-07-26 20:47:16 +0700

    Update backend code
    Deploy backend to digital ocean

--- core-api/src/payment/payment.module.ts
+++ core-api/src/payment/payment.module.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { PaymentService } from './payment.service';
 import { PaymentController } from './payment.controller';
