7ea18c6aadd1dbc508ca5b2772c95432be7104b8
Dương Quang Vinh <duongquangvinh6@gmail.com>
2022-08-13 13:10:55 +0700

    Update Header.tsx

--- Frontend__Client/components/Header.tsx
+++ Frontend__Client/components/Header.tsx
@@ -10,11 +10,14 @@ import Avatar from '@mui/material/Avatar'
 import Link from 'next/link'
 import Divider from '@mui/material/Divider'
 import styles from '../styles/Header.module.scss'
+import PlayCircleOutlineOutlinedIcon from '@mui/icons-material/PlayCircleOutlineOutlined'
+import ReplayOutlinedIcon from '@mui/icons-material/ReplayOutlined'
+
 interface HeaderProps {
-	props: any;
+	props: any
 }
 interface TicketProps {
-	data: TicketProp[];
+	data: TicketProp[]
 	// conferenceOrganizer: string;
 }
 interface TicketProp {
@@ -27,13 +30,13 @@ interface TicketProp {
 	// conferenceOrganizer: string;
 }
 const Header = (props: any) => {
-	const [ticketList, setTicketList] =  useState<TicketProps>()
+	const [ticketList, setTicketList] = useState<TicketProps>()
 	const pages = ['Products', 'Pricing', 'Blog']
 	const settings = ['Profile', 'Account', 'Dashboard', 'Logout', 'Account', 'Dashboard', 'Logout', 'Account', 'Dashboard', 'Logout']
 
 	const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null)
 	const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null)
-// 12:00 am - Thu, Jul 1
+	// 12:00 am - Thu, Jul 1
 	function parseDate(date: Date) {
 		date = new Date(date)
 		const day = date.getDate()
@@ -41,8 +44,8 @@ const Header = (props: any) => {
 		const year = date.getFullYear()
 		const hour = date.getHours()
 		const min = date.getMinutes()
-		let ampm = hour >= 12 ? 'pm' : 'am';
-		const weekday = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
+		let ampm = hour >= 12 ? 'pm' : 'am'
+		const weekday = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
 		let weekDayString = weekday[date.getDay()]
 		let monthString = date.toLocaleString('en-us', { month: 'short' })
 		const dateString = `${hour}:${min} ${ampm} - ${weekDayString}, ${monthString} ${day}`
@@ -55,7 +58,7 @@ const Header = (props: any) => {
 				const cateResult = await dataResult.json()
 				setTicketList(cateResult)
 			}
-			fetchTicketList();
+			fetchTicketList()
 		}
 	}, [props?.tempDecode?.role, props?.tempDecode?.sub])
 
@@ -148,34 +151,43 @@ const Header = (props: any) => {
 								open={Boolean(anchorElUser)}
 								onClose={handleCloseUserMenu}
 							>
-								{ticketList?.data.length === 0 || ticketList === undefined && (
-									<>
-									<MenuItem key={0} onClick={handleCloseUserMenu} sx={{ mb: '0.5rem', userSelect: 'none' }}>
-										<Box
-											sx={{ width: '400px', height: '75px', display: 'flex', gap: '2rem', alignItems: 'center' }}
-										>
-											<Typography component='h3'>You dont have any tickets</Typography>
-										</Box>
-									</MenuItem>
-									<Divider variant='inset' component='li' sx={{ width: '100%', mx: '0 !important' }} />
-								</>
-								)}
+								{ticketList?.data.length === 0 ||
+									(ticketList === undefined && (
+										<>
+											<MenuItem key={0} onClick={handleCloseUserMenu} sx={{ mb: '0.5rem', userSelect: 'none' }}>
+												<Box
+													sx={{
+														width: '400px',
+														height: '75px',
+														display: 'flex',
+														gap: '2rem',
+														alignItems: 'center',
+													}}
+												>
+													<Typography component='h3'>You dont have any tickets</Typography>
+												</Box>
+											</MenuItem>
+											<Divider variant='inset' component='li' sx={{ width: '100%', mx: '0 !important' }} />
+										</>
+									))}
+
 								{ticketList?.data?.map((ticket) => (
 									<>
 										{' '}
-										<MenuItem key={ticket?.conference_id} onClick={handleCloseUserMenu} sx={{ mb: '0.5rem', userSelect: 'none' }}>
-											<Box
-												sx={{ width: '400px', height: '75px', display: 'flex', gap: '2rem', alignItems: 'center' }}
-											>
-												<Box sx={{ width: '25%', height: '100%' }}>
-													<img
-														width={100}
-														height={75}
-														src={
-															'https://images.pexels.com/photos/10402422/pexels-photo-10402422.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
-														}
-													/>
-												</Box>
+										<MenuItem
+											key={ticket?.conference_id}
+											onClick={handleCloseUserMenu}
+											sx={{ mb: '0.5rem', userSelect: 'none' }}
+										>
+											<Box sx={{ width: '450px', height: '90px', display: 'flex', gap: '0.5rem' }}>
+												<img
+													width='25%'
+													height='100%'
+													src={
+														'https://images.pexels.com/photos/10402422/pexels-photo-10402422.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+													}
+												/>
+
 												<Box
 													className={styles.notification_Content}
 													sx={{
@@ -185,22 +197,20 @@ const Header = (props: any) => {
 														alignContent: 'center',
 														flexDirection: 'column ',
 														justifyContent: 'center',
+														gap: '0.5rem',
 													}}
 												>
 													<Typography component='h3'>{ticket?.conference_name}</Typography>
 													<Typography component='h4'>{parseDate(ticket?.date_start_conference)}</Typography>
 												</Box>
-												<Box
-													display='flex'
-													flexDirection='column'
-													flexGrow={1}
-													sx={{ width: '25%', alignItems: 'flex-start' }}
-												>
-													<IconButton>
-														<NotificationsNoneIcon /> Join
+												<Box display='flex' flexDirection='column' sx={{ width: '25%', alignItems: 'flex-start' }}>
+													<IconButton sx={{ display: 'flex', gap: '0.5rem', color: '#C64EFF' }}>
+														<PlayCircleOutlineOutlinedIcon />
+														<Typography>Join</Typography>
 													</IconButton>
-													<IconButton>
-														<NotificationsNoneIcon /> Record
+													<IconButton sx={{ display: 'flex', gap: '0.5rem', color: '#C64EFF' }}>
+														<ReplayOutlinedIcon />
+														<Typography>Record</Typography>
 													</IconButton>
 												</Box>
 											</Box>
