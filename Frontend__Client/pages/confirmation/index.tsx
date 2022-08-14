71cd0047d260fb92e2331eef041e930bbb9f5775
Đỗ Nguyễn Minh Thành <minhthanhd013@gmail.com>
2022-08-14 14:46:49 +0700

    Update index.tsx

--- Frontend__Client/pages/confirmation/index.tsx
+++ Frontend__Client/pages/confirmation/index.tsx
@@ -1,13 +1,25 @@
 import React, { useEffect } from "react";
 import Footer from "../../components/Footer";
-import Header from "../../components/Header";
+import Header from "../../components/Header_Confirm";
+import Typography from '@mui/material/Typography';
+import styles from '../../styles/Event.module.scss';
+import { useRouter } from "next/router";
 
 const Confirmation = (prop: any) => {
-    
+  
+  const router = useRouter();
+  const redirect = () => {
+    router.push("/host/dashboard")
+  }
+  setTimeout(redirect,5000);
+
     return (
         <>
-            <Header {...prop}/>
-            {console.log(prop)}
+            <Header/>
+            <Typography variant="h1" component="div" gutterBottom className={styles.confirmation}>
+        		  Your payment has been done succesfully
+      		  </Typography>
+            
             <Footer/>
         </>
     );
