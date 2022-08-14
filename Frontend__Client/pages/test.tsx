d0507f16e13eb4ac433e31481ff876d1a6ef499e
Dương Quang Vinh <duongquangvinh6@gmail.com>
2022-08-15 03:10:50 +0700

    Update test.tsx

--- Frontend__Client/pages/test.tsx
+++ Frontend__Client/pages/test.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
+import Combo__2 from '../components/Combo__2'
 import PurchaseModal from '../components/PurchaseModal'
 import TicketList_SearchResult from '../components/TicketList__SearchResult'
 import SearchResult from './search-result'
 const Test = () => {
 	return (
 		<>
-			{/* <SearchResult /> */}
-			<PurchaseModal/>
+			<Combo__2 data={undefined} props={undefined}/>
 		</>
 	)
 }
